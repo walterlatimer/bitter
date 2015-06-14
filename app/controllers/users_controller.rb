3a55e4149e31272bf78b6dcfe17a9eea3d87ca98
walterlatimer <walter@walterlatimer.com>
2015-06-14 13:41:13 -0400

    Add registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to register_path
+    end
+
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :bio)
+  end
+
+
+
+end
